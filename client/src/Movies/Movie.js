3242c36a5b8a5769dccaf4e5f363b9cf233876da
stevenoakley211 <stevenoakley211@live.com>
2020-04-30 22:05:55 -0500

    mvp + stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,48 +1,61 @@
-import React from "react";
-import axios from "axios";
-import MovieCard from "./MovieCard";
-export default class Movie extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movie: null
-    };
-  }
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useRouteMatch, useHistory } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
-  componentDidMount() {
-    this.fetchMovie(this.props.match.params.id);
-  }
+function Movie(props) {
+  console.log(props);
+  const [movie, setMovie] = useState(null);
+  const match = useRouteMatch();
+  const history = useHistory();
 
-  componentWillReceiveProps(newProps) {
-    if (this.props.match.params.id !== newProps.match.params.id) {
-      this.fetchMovie(newProps.match.params.id);
-    }
-  }
-
-  fetchMovie = id => {
+  const fetchMovie = id => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
-      .then(res => this.setState({ movie: res.data }))
+      .then(res => setMovie(res.data))
       .catch(err => console.log(err.response));
   };
 
-  saveMovie = () => {
-    const addToSavedList = this.props.addToSavedList;
-    addToSavedList(this.state.movie);
+  const saveMovie = () => {
+    props.addToSavedList(movie);
   };
 
-  render() {
-    if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
-    }
+  const routeToEditForm = e => {
+    e.preventDefault();
+    history.push(`/update-movie/${movie.id}`)
+  };
 
-    return (
-      <div className="save-wrapper">
-        <MovieCard movie={this.state.movie} />
-        <div className="save-button" onClick={this.saveMovie}>
-          Save
-        </div>
-      </div>
-    );
+  const deleteMovie = e => {
+    e.preventDefault();
+    axios
+    .delete(`http://localhost:5000/api/movies/${movie.id}`)
+    .then(res => {
+      console.log(res);
+      props.setMovieList(res.data);
+      props.history.push('/');
+    })
+    .catch(err => console.log(err));
+  };
+
+  
+
+  useEffect(() => {
+    fetchMovie(match.params.id);
+  }, [match.params.id]);
+
+  if (!movie) {
+    return <div>Loading movie information...</div>;
   }
+
+  return (
+    <div className='save-wrapper'>
+      <MovieCard movie={movie} />
+
+      <div className='save-button' onClick={saveMovie}> Save </div>
+      <div onClick={routeToEditForm}> Edit </div>
+      <div onClick={deleteMovie}>Delete</div>
+    </div>
+  );
 }
+
+export default Movie;
